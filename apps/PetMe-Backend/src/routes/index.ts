28d41cdf4f485bcec4a627b7462dabde1b7bddb8
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-06-06 13:50:57 +0530

    [FEAT] Added Pet Routes to the structure

--- apps/PetMe-Backend/src/routes/index.ts
+++ apps/PetMe-Backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import VolunteerRoutes from './VolunteerUserRoutes';
 import UserRoutes from './UserRoutes';
 import BlogRoutes from './BlogRoutes';
+import PetRoutes from './PetRoutes';
 
 const router = Router();
 
 router.use('/api/v1/volunteer', VolunteerRoutes);
 router.use('/api/v1/user', UserRoutes);
 router.use('/api/v1/blog', BlogRoutes);
+router.use('/api/v1/pet', PetRoutes);
 
 export default router;
