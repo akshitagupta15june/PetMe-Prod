74333f68036ed908fee60b8eb7970aecdc8488d2
Krish <prathamrajsri2003@gmail.com>
2024-06-09 17:44:26 +0530

    SOS endpoint created

---
+++ apps/PetMe-Backend/src/interfaces/Pet/SosPets.ts
@@ -0,0 +1,9 @@
+export default interface SosPets {
+    id: string,
+    report_user_id: string,
+    landmark: string,
+    type_id: string,
+    petCondition: string,
+    additionalDesc: string,
+    typeId: string
+}
