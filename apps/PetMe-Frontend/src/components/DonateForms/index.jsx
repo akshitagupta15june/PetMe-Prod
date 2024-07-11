5e6830390f30fdc4fb2e0ee73b510bc32ef853c8
Mehak <mehakmattoo7@gmail.com>
2024-07-11 14:42:50 +0530

    styled donate animal page

--- apps/PetMe-Frontend/src/components/DonateForms/index.jsx
+++ apps/PetMe-Frontend/src/components/DonateForms/index.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import FormsInput from './components/FormsInput';
-import inputs from '../../helpers/inputsForDonateForms';
+import React from "react"
+import FormsInput from "./components/FormsInput"
+import inputs from "../../helpers/inputsForDonateForms"
+import cat from "../../assets/animalsImages/cat-annoyed.png"
+import dog from "../../assets/animalsImages/dog-happy.png"
 
 function DonateForms() {
   return (
-    <section className="flex flex-col items-center gap-10 p-10 w-full">
+    <section className=" bg-pink-200 flex flex-col items-center  w-full">
+      <div className="items-center justify-center relative flex">
+        <p className="bg-red-400 absolute left-10 sm: text-xs md:text-sm font-bold text-white p-2 rounded-full w-fit ">
+          Yes !! I want new frens
+        </p>
+        <img src={dog} alt="Dog" className="w-full sm:w-1/3 lg:w-2/3" />
+      </div>
 
       <div className="text-center my-10">
-
-        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-          Donate Animals
-        </h1>
-        <p className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-90">
+        <p className="sm:text-4xl font-black text-4xl title-font mb-4 text-pink-600">
           Fill up the form below and donate your pets.
         </p>
-
       </div>
 
       <form
@@ -23,21 +26,18 @@ function DonateForms() {
         method="POST"
         className="w-full lg:grid lg:grid-cols-2 gap-4 lg:w-1/2 md:w-2/3"
       >
-        { inputs
-          .map((inputText, index) => <FormsInput text={inputText} key={inputText} index={index} />)}
+        {inputs.map((inputText, index) => (
+          <FormsInput text={inputText} key={inputText} index={index} />
+        ))}
       </form>
 
       <div className="text-center w-full">
-
         <label htmlFor="message" className="leading-7 text-xl text-gray-900">
           Your Pet Image
         </label>
 
         <div className="max-w-3xl mx-auto mt-5">
-
-          <label
-            className="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none"
-          >
+          <label className="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
             <span className="flex items-center space-x-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -61,34 +61,28 @@ function DonateForms() {
             </span>
 
             <input type="file" name="file_upload" className="hidden" />
-
           </label>
         </div>
       </div>
 
       <div>
-
         <input type="checkbox" name="check" id="check" required />
 
         <span className="p-1 font-medium text-gray-900">
           I have read and agree to the Terms and Conditions of PetMe.
         </span>
-
       </div>
 
       <div>
-
         <button
           type="submit"
           className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
         >
           Submit
         </button>
-
       </div>
-
     </section>
-  );
+  )
 }
 
-export default DonateForms;
+export default DonateForms
